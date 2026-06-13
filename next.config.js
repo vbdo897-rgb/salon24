fff5537e1c25db1efeb539cd494f1d88deb29acb
vbdo8 <salon24m@gmail.com>
2026-06-14 02:50:18 +0300

    fix pwa config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
-
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-});
+};
+
+module.exports = nextConfig;
